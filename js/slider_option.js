b1af16f43d02d4e1597d8220b49b1dcd5a4a02ca
seulgi5041 <crudel44@gmail.com>
2021-11-11 19:02:14 +0900

    img수정

--- js/slider_option.js
+++ js/slider_option.js
@@ -1,7 +1,7 @@
-var swiper1 = new Swiper('.swiper-container1', {
+var swiper1 = new Swiper('.swiper-container', {
     slidesPerView: 5,
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
     },
-  });
+});
