249814ea67ba32e5ddbe779cb1a588c155968582
seulgi5041 <crudel44@gmail.com>
2021-11-12 18:40:37 +0900

    메인헤더_스와이퍼css수정중

--- js/slider_option.js
+++ js/slider_option.js
@@ -1,7 +1,26 @@
-var swiper1 = new Swiper('.swiper-container', {
+var swiper = new Swiper('.swiper', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+
+var swiper1 = new Swiper('.swiper1', {
     slidesPerView: 5,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
 });
+
