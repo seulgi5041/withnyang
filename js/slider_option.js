bb7ad022d5f61a4a1a07ea42aba3022a7425dad6
seulgi5041 <crudel44@gmail.com>
2021-11-16 22:44:52 +0900

    responsive

--- js/slider_option.js
+++ js/slider_option.js
@@ -15,26 +15,53 @@ var swiper = new Swiper('.swiper', {
 });
 
 
-var swiper1 = new Swiper('.swiper1', {
-    slidesPerView: 5,
+var swiper1 = new Swiper('.tab01 .swiper1', {
+    slidesPerView: 5, 
+    pagination: {
+        el: '.tab01 .swiper-pagination',
+    },
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.tab01 .swiper-button-next',
+        prevEl: '.tab01 .swiper-button-prev',
     },
 });
 
-var swiper2 = new Swiper('.swiper2', {
+var swiper2 = new Swiper('.tab02 .swiper2', {
     slidesPerView: 5,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+    pagination: {
+        el: '.tab02 .swiper-pagination',
     },
+    navigation: {
+        nextEl: '.tab02 .swiper-button-next',
+        prevEl: '.tab02 .swiper-button-prev',
+    },
+
+    observer: true,
+    observeParents: true,
 });
 
-var swiper3 = new Swiper('.swiper3', {
+var swiper3 = new Swiper('.tab03 .swiper3', {
     slidesPerView: 5,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+    pagination: {
+        el: '.tab03 .swiper-pagination',
     },
+    navigation: {
+        nextEl: '.tab03 .swiper-button-next',
+        prevEl: '.tab03 .swiper-button-prev',
+    },
+
+    observer: true,
+    observeParents: true,
+});
+
+
+// 아이콘 탭 클릭시 div 컨텐츠 변화
+$(".tab-wrapper .icon_box").on('click', function () {
+
+    $(".tab-wrapper .icon_box").removeClass("on");
+    $(this).addClass("on");
+    $(".tab-contents").hide();
+    var tabid = $(this).attr("rel");
+    $("." + tabid).fadeIn();
+
 });
