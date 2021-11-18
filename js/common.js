c5d21d9d31fb5b4d8cbe8559f56fa46fe264ed53
seulgi5041 <crudel44@gmail.com>
2021-11-18 15:45:39 +0900

    plusBtn

--- js/common.js
+++ js/common.js
@@ -21,15 +21,37 @@ $(function (){
 });
 
 
-// chatBtn 버튼 누르면 위에 버튼 나옴
+// plusBtn 버튼 누르면 위에 버튼 나옴
 $(function(){
     $('.plusBtn').click(function(){
+
         $('.bookBtn').toggle();
         $('.mapBtn').toggle();
         $('.chatBtn').toggle();
     
     });
 });
+
+
+
+
+//toggle 이벤트로 기능 부여
+// $(document).ready(function(){
+//         btn = $('.plusBtn'); //버튼 아이디 변수 선언
+//         layer = $('.chatBtn'); //레이어 아이디 변수 선언
+
+//         btn.click(function(){
+//             layer.toggle(function(){
+//                 layer.addClass('.plus_show')
+//             }, //클릭하면 show클래스 적용되서 보이기
+
+//             function(){
+//                 layer.addClass('.plus_hide')
+//             } //한 번 더 클릭하면 hide클래스가 숨기기
+//         );
+//     });
+// });
+
 
 
 // 푸터 언어창
